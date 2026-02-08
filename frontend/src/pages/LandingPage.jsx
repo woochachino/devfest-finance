0d0f288e3d93b2ee021dc9b6fbb56e97b38c05e5
Nino Chkhaidze <chkhaidzenintsi@gmail.com>
2026-02-08 06:03:40 -0500

    editede leaderboard

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -351,7 +351,7 @@ export default function LandingPage() {
                         </p>
                     </div>
 
-                    <div className="grid md:grid-cols-2 gap-8">
+                    <div className="grid md:grid-cols-2 gap-8 text-left">
                         {/* Global Leaderboard */}
                         <div className="space-y-4">
                             <div className="flex items-center justify-between px-2">
